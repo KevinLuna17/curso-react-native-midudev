4b96f3bf1fc78031606c3cf3e30bd05dbdf5d6f1
KevinLuna17 <kelugo_14@hotmail.com>
2024-08-15 17:30:09 -0500

    Add code

--- App.js
+++ App.js
@@ -1,28 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-
-const icon = require('./assets/icon.png');
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Main } from "./components/Main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Image source={{ uri: "https://wallpapercave.com/wp/wp11383569.jpg" }} style={{width: 300, height: 200 }} />
-      <Text style={{color: '#fff'}}>Tenemos aqui la app!</Text>
-      <Button 
-      title='Pulsa aqui' 
-      onPress={()=> alert('Hola')} 
-      />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 12,
   },
-
 });
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Descripción del proyecto
 
-Metacritic es una aplicacion para poder chequear las reseñas y valoraciones de algunos productos.
+Metacritic es una aplicacion para poder chequear las reseñas y valoraciones de algunos juegos, peliculas.
 
 ## Características
 
@@ -23,6 +23,31 @@ Permitir que el proyecto se pueda ejecutar en web:
 Instalar eslint en el proyecto:
 
     npx expo lint
+    npm install --save --save-dev prettier eslint-config-prettier eslint-plugin-prettier
+
+Configurar eslint:
+
+    En el archivo .eslintrc.js debemos de agregar lo siguiente:
+    module.exports = {
+        extends: ['expo', 'prettier'],
+        plugins: ['prettier'],
+        rules: {
+            'prettier/prettier': 'error',
+        },
+    };
+    Tener las extensiones de eslint y prettier code formater en el Vs Code instalado.
+
+Instalar constantes de expo para acceder a constantes de la aplicación:
+
+    npx expo install expo-constants
+
+Instalar dependencia para que las aplicaciones se visualicen bien en referencia al statusBarHeight:
+
+    npx expo install react-native-safe-area-context
+
+Para que rendericen los svg en la aplicacion:
+
+    npx expo install react-native-svg
 
 ## ✔️ Tecnologías
 
